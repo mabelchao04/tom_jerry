e7fb0e3c4ce957019c275f29f43ce95b2e51f07f
mabelchao04 <mabelaxiay@gmail.com>
2021-04-02 10:08:11 +0800

    updated Animal.php

--- app/Animal.php
+++ app/Animal.php
@@ -4,11 +4,15 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Animal extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
+    protected $dates = ['deleted_at'];
+    
     /*
      * 可以被批量寫入的屬性
      * 
