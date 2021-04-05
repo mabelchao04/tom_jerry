d733591bf31c88df4d75f7939e000d4c78cb4795
mabelchao04 <mabelaxiay@gmail.com>
2021-04-05 23:20:13 +0800

    updated validate update App\Http\Controller\AnimalController.php

--- app/Http/Controllers/AnimalController.php
+++ app/Http/Controllers/AnimalController.php
@@ -123,6 +123,18 @@ class AnimalController extends Controller
      */
     public function update(Request $request, Animal $animal)
     {
+        $this->validate($request,[
+            'type_id' => 'nullable|integer',
+            'name' => 'string|max:255',
+            'birthday' => 'nullable|date',
+            'area' => 'nullable|string|max:255',
+            'fix' => 'boolean',
+            'description' => 'nullable|string',
+            'personality' => 'nullable|string'
+        ]);
+
+        $request['user_id'] = '1';
+
         $animal->update($request->all());
         return response($animal, Response::HTTP_OK);
     }
