665ae957615abd9fc7b4dcca2dffc8adb005da7c
mabelchao04 <mabelaxiay@gmail.com>
2021-04-29 01:12:11 +0800

    Added parallax

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,43 @@
 @extends('layouts.app')
 
+@section('content')
+<!-- First Parallax Section -->
+    <div class="jumbotron paral paralsec">
+        <h1 class="display-3">Here is a heading 1</h1>
+        <p class="lead">Here is a short description 1</p>
+        <p class="lead">
+            <a class="btn btn-info btn-lg btn-md" href="#" role="button">Here is a button 1</a>
+        </p>
+    </div>
+
+    <!-- Second Parallax Section -->
+    <div class="jumbotron paral paralsec1">
+        <h1 class="display-3">Here is a heading 2</h1>
+        <p class="lead">Here is a short description 2</p>
+        <p class="lead">
+            <a class="btn btn-warning btn-lg btn-md" href="widgets.html" role="button">Here is a button 2</a>
+        </p>
+    </div>
+
+    <!-- Third Parallax Section -->
+    <div class="jumbotron paral paralsec2">
+        <h1 class="display-3">Here is a heading 3</h1>
+        <p class="lead">Here is a short description 2</p>
+        <p class="lead">
+            <a class="btn btn-primary btn-lg btn-md" href="themes.html" role="button">Here is a button 3</a>
+        </p>
+    </div>
+            
+    <!-- Add More Parallax Sections Here -->
+
+    <!-- Footer Section -->
+    <footer class="wn-footer">
+        <p>This is a Bootstrap 4 parallax page with jumbotron created by <a href="https://www.webnots.com/">WebNots Web Consulting Services</a>
+        </p>
+        <p><a href="#">Back to top</a></p>
+    </footer>
+@endsection
+
 @section('dashboard')
 <div class="container">
     <div class="row justify-content-center">
